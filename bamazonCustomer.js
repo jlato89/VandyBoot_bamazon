115dd1e1ee3f219d7d224583a5c557119b92e9fb
Josh Latour <jlatour88@gmail.com>
2019-07-18 23:49:06 -0500

    fixed tab spacing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,11 @@ var inquirer = require('inquirer');
 
 //* MYSQL CONNECTION
 var connection = mysql.createConnection({
-host: 'localhost',
-port: 3306,
-user: 'root',
-password: 'password',
-database: 'bamazon_db'
+   host: 'localhost',
+   port: 3306,
+   user: 'root',
+   password: 'password',
+   database: 'bamazon_db',
 });
 
 connection.connect(function (err) {
